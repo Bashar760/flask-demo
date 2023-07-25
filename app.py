738c2ee58d30beb9ff78524b7b4c9508e9c4bee9
Bashar <bashar750@gmail.com>
2023-07-25 20:19:42 +0300

    Documentation

--- app.py
+++ app.py
@@ -1,26 +1,91 @@
 # """Simple Flask Project"""
 
 # source: https://www.youtube.com/watch?v=0Qxtt4veJIc&list=PLCC34OHNcOtolz2Vd9ZSeSXWc8Bq23yEz&index=1
+# source: https://www.youtube.com/watch?v=mqhxxeeTbu0&list=PLzMcBGfZo4-n4vJJybUVV3Un_NFS5EOgX
+
 
 # import logging
+from enum import unique
 from wsgiref.validate import validator
-from flask import Flask, render_template, flash, request, redirect, url_for
+from flask import Flask, render_template, flash, request, redirect, url_for, session
 from markupsafe import escape
-
-# from flask_bootstrap import Bootstrap
 
 # Load forms libraries
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-app = Flask(__name__)
-# Bootstrap(app)
+# Import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
 
-# Create CSRF Token to be used by FORMS
+app = Flask(__name__)
+
+# Refer to Flask CONFIG Documentation: https://flask.palletsprojects.com/en/2.3.x/config/
+# Create CSRF Token to be used by FORMS, Sessions and Cookies
 app.secret_key = "please_use_a_COMPLEX_secret_key"
 #  OR
 # app.config['SECRET_KEY'] = "please_use_a_COMPLEX_secret_key"
+
+# Optionally define session lifetime otherwise
+# session will be terminated when browser is closed
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+# Point database url to local directory
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+
+# Initialize database
+db = SQLAlchemy(app)
+
+# Create model
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, name=None, email=None):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return f"<Name: {self.name!r}>"
+
+"""
+Create model using shell
+(venv) PS D:\flask> flask shell
+>>> from app import db
+>>> db.create_all()
+>>> exit()
+"""
+
+# Create a Form Class
+class UserForm(FlaskForm):
+    name = StringField("Name?", validators=[DataRequired()])
+    email = StringField("Email?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route("/user/add", methods=["GET", "POST"])
+def add_user():
+    name = None
+    form = UserForm()
+
+    # Validate Form
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+
+        form.name.data = ""
+        form.email.data = ""
+
+        flash("User added successfully", "success")
+
+    return render_template("add_user.html", form=form, name=name)
 
 
 # Create a Form Class
@@ -28,7 +93,7 @@ class NamerForm(FlaskForm):
     name = StringField("What is you name?", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
-
+"""
 # FIELDS TYPES
 # BooleanField
 # DateField
@@ -63,7 +128,7 @@ class NamerForm(FlaskForm):
 # UUID
 # AnyOf
 # NoneOf
-
+"""
 
 @app.route("/hello")
 def hello():
@@ -71,16 +136,23 @@ def hello():
 
 
 @app.route("/")
-def index():
+def home():
     pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
     context = {"name": "Bashar", "lname": "Horanieh", "pizza": pizza}
     return render_template("index.html", context=context)
 
 
-@app.route("/user/<name>")
-def user(name):
+@app.route("/players/<name>")
+def players(name):
     # Always escape untrusted variables
     return f"Hello, {escape(name).capitalize()}!"
+
+
+@app.route("/admin")
+def admin():
+    # Enter the name of the FUNCTION to redirect to
+    return redirect(url_for("home"))
+
 
 @app.route("/log")
 def log():
@@ -88,6 +160,52 @@ def log():
     # app.logger.warning('A warning occurred (%d apples)', 42)
     # app.logger.error('An error occurred')
     return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        # session.permanent = True
+        # user = request.form.get("name")
+        user = request.form["name"]
+        session["user"] = user
+        # flash("Successfully logged in", "success")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+        
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    flash("Successfully logged out", "success")
+    session.pop("user", None)
+    session.pop("email", None)
+    return render_template("login.html")
+    
+
+@app.route("/user", methods=["GET", "POST"])
+def user():
+    email = None
+    
+    if "user" in session:
+        user = session["user"]
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+        else:
+            if "email" in session:
+                email = session["email"]
+                
+            
+        return render_template("user.html", name=user, email=email)
+    else:
+        flash("You are not logged in!")
+        # return render_template("login.html")
+        return redirect(url_for("login"))
+    
 
 
 @app.route("/name", methods=["GET", "POST"])
@@ -99,7 +217,7 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ""
-        flash("You were successfully logged in", "success")
+        flash("You request was completed..")
 
     return render_template("name.html", name=name, form=form)
 
@@ -115,16 +233,18 @@ def internal_server_error(e):
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run()
 
+
+"""
+# app.run(debug=True)
 # PS D:/flask> set FLASK_ENV=development
 # PS D:/flask> set FLASK_App=app.py
-
-# Following command will refresh the server automatically
-
-# PS D:/flask> flask --app example_app.py --debug run
-
+# PS D:/flask> flask --debug run
+# db.create_all()
 # Following two commands can be used to save Powershell history
 
 # PS D:/flask> Get-History | Export-Csv history.csv
 # PS D:/flask> Get-History | Out-File history.txt
+"""
---
+++ static/js/test.js
@@ -0,0 +1,6 @@
+/* Just a blank js file */
+const o = document.getElementById("demo");
+
+if (o) {
+    o.innerHTML = "This text was created by JS";
+}
